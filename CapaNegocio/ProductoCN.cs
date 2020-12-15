9f0705134bdf6eef8e02ce26704f481157e071cd
Wilmer <Wilmer@DESKTOP-BKHUJQ5>
2020-12-15 10:29:34 -0500

    Se agrego capa negocios

--- CapaNegocio/ProductoCN.cs
+++ CapaNegocio/ProductoCN.cs
@@ -3,11 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CapaEntidad;
+using CapaDatos;
 namespace CapaNegocio
 {
     class ProductoCN
     {
         // Metodos equivalentes a la capa DATOS
+
+        public ProductoCE buscarId(int idBuscado)
+        {
+
+            // Instanciar la capa datos
+            ProductoCD productoCD = new ProductoCD();
+            
+            // Ejecutar el metodo buscarid de la capa de datos
+            ProductoCE producto = productoCD.buscarId(idBuscado);
+
+            // retornar el resultado
+            return producto;
+        }
+        public List<ProductoCE> buscarDescripcion(string descripcion)
+        {
+            // Instanciar la capa datos
+            ProductoCD productoCD = new ProductoCD();
+
+            // Ejecutar el metodo buscarDescripcion de la capa datos
+            List<ProductoCE> productos = productoCD.buscarDescripcion(descripcion);
+
+            // retornar el resultado
+            return productos;
+        }
+
+        public int insertar(ProductoCE productoCE)
+        {
+            // Instanciar capa datos
+            ProductoCD productoCD = new ProductoCD();
+            // Ejecutar metodo insertar
+            int numFila = productoCD.insertar(productoCE);
+            // Retornar numero de filas afectadas
+            return numFila;
+        }
+        public int actualizar(ProductoCE productoCE)
+        {
+            // Instanciar capa datos
+            ProductoCD productoCD = new ProductoCD();
+            // Ejecutar metodo actualizar
+            int numFila = productoCD.actualizar(productoCE);
+            // Retornar numero de filas afectadas
+            return numFila;
+        }
+        public int eliminar(ProductoCE productoCE)
+        {
+            // Instanciar capa datos
+            ProductoCD productoCD = new ProductoCD();
+            // Ejecutar metodo eliminar
+            int numFila = productoCD.eliminar(productoCE);
+            // Retornar numero de filas afectadas
+            return numFila;
+        }
     }
 }
